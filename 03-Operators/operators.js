4793554a5ad74c0a4521c564590648ec5be6a5ad
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-07-22 15:45:16 +0530

    Js operators practice

---
+++ 03-Operators/operators.js
@@ -0,0 +1,164 @@
+// Arithmatic operators
+
+    // let a=2;
+    // let b = 3;
+    // console.log("a=",a);
+    // console.log("b=",b);
+    // console.log("addition", a+b);
+    // console.log("Subtraction", a-b);
+    // console.log("multiplaction", a*b);
+    // console.log("dividion", a/b);
+    // console.log("modules",a%b);
+
+
+
+// Logical Operators && ,||,!
+
+    // both conditions should be true
+            // if(12>13 && 13>12){
+            //     console.log("Valid");
+
+            // }else{
+            //     console.log("InValid");
+
+            // }
+
+
+    // any one of both conditions should be true
+
+            // if(12>13 || 13>12){
+            //     console.log("Valid");
+
+            // }else{
+            //     console.log("InValid");
+
+            // }
+
+
+    // ! not
+            // if(!(12>13)){
+            //     console.log("valid");
+            // }else{
+            //     console.log("invalid");
+            // }
+
+
+// Assignment operator
+    // let a=2;
+    // let b = 3;
+    // a+=b;
+    // console.log(a);
+    //  a-=b;
+    //  console.log(a);
+    //  a*=b;
+    // console.log(a);
+    //  a/=b;
+    // console.log(a);
+    // a%=b;
+    // console.log(a);
+
+
+
+// increament and decrement
+
+    // let x=5;
+    // console.log(++x);
+    // console.log(x++);
+    // console.log(x);
+
+
+    // let x=5;
+    // console.log(--x);
+    // console.log(x--);
+    // console.log(x);
+
+
+    // let y=8;
+    // console.log(y++ + ++y);
+
+    // let y=8;
+    // console.log(y-- + ++y);
+
+ 
+// Comparison Operators
+
+    //  let x=12;
+    //  let y=15;
+    // console.log(x>y);
+    // console.log(x<y);
+    // console.log(x>=y);
+    // console.log(x<=y);
+    // console.log(x!=y);
+    // console.log(x===y);
+    // console.log(x==y);
+
+
+
+// Ternary Operator
+
+    // let age =19;
+    // age>=18? console.log("eligible") :console.log(" not eligible");
+
+
+    // let score=77;
+    // score>=90 ? console.log("A") : score>=75? console.log("B") :score>=50? console.log("B") : console.log("fail") ;
+
+   
+
+// Unary Operators
+
+// Unary = Works on one value
+// + → Convert to Number
+// - → Make Negative
+// ++ → Add 1
+// -- → Subtract 1
+// Pre (++a, --a) → Change first, then use
+// Post (a++, a--) → Use first, then change
+
+
+
+        // let a="3";
+        // console.log(!!a);
+
+        // let a="30";
+        // console.log(-a);
+        // console.log(+a);
+        // console.log(++a);
+        // console.log(a++);
+        // console.log(+a);
+        // console.log(--a);
+        // console.log(a--);
+        // console.log(+a);
+        // console.log(!a);
+        // console.log(!!a);
+
+
+// Bitwise operators
+// &	AND	
+// |	OR
+// ^	XOR	
+// ~	NOT	
+// <<	Left Shift	
+// >>	Right Shift	
+
+        //  let a=3;
+        //  let b=4;
+        // console.log(a & b);
+        // console.log(a | b);
+        // console.log(a^ b);
+        // console.log(~b);
+        // console.log(b<<1);
+        // console.log(b>>1);
+
+
+
+
+
+
+
+
+
+
+
+
+
