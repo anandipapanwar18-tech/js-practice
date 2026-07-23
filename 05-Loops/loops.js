414f8515ddf53073f329711c8edd808307b4724b
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-07-23 23:39:20 +0530

    Practiced Loop Exampleas

---
+++ 05-Loops/loops.js
@@ -0,0 +1,22 @@
+// printing even number from 1 to 20 using for loop
+
+
+// for(let i=1; i<=20; i++){
+//     if(i%2==0){
+//         console.log(i ,"is Even number");
+//     }
+// }
+
+
+// printing odd number from 1 to 20 using while loop
+
+let i=1;
+
+while(i<=20){
+    if(i % 2!=0){
+        console.log(i ,"is odd number");
+        
+    }
+         i++;
+
+}
