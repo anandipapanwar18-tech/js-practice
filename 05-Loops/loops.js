398daeb2c3a925aa0bb6fa667ee3e07afa374b0e
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-07-24 16:35:52 +0530

    practiced loop examples

--- 05-Loops/loops.js
+++ 05-Loops/loops.js
@@ -10,13 +10,31 @@
 
 // printing odd number from 1 to 20 using while loop
 
-let i=1;
+// let i=1;
 
-while(i<=20){
-    if(i % 2!=0){
-        console.log(i ,"is odd number");
+// while(i<=20){
+//     if(i % 2!=0){
+//         console.log(i ,"is odd number");
         
-    }
-         i++;
+//     }
+//          i++;
 
-}
+// }
+
+// multiplaction of 5 using for and while loop
+
+
+// for(let i= 1;i<=10;i++){
+//     let a=5*i;
+//     console.log("5 *", i,"=",a);
+// }
+
+
+// let i=1;
+// while(i<=10){
+//     let a=5*i;
+//     console.log("5 *", i,"=",a);
+//     i++;
+// }
+
+
