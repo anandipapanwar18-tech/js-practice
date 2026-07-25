cb8b9cf86b38431680af55edc4c30a1baa03bbfa
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-07-25 22:49:05 +0530

    Practicedd More example of Loops

--- 05-Loops/loops.js
+++ 05-Loops/loops.js
@@ -38,3 +38,69 @@
 // }
 
 
+// let i=1;
+// let a=0;
+
+// while(i<=100){
+//     a+=i;
+//     i++
+// }
+// console.log(a);
+
+
+
+// number between 1 to 50 that are divisible by 3
+// for(let i=1;i<=50;i++){
+//     if(i%3==0){
+//         console.log(i);
+//     }
+// }
+
+
+
+//  take a number from user and print the even and odd from 1 to that number 
+// using for loop
+        // let n=prompt("Enter the number");
+        // for(i=1;i<=n;i++){
+        //     if(i%2==0){
+        //         console.log(i ,"Is Even number");
+        //     }else{
+        //          console.log(i ,"Is odd number");
+
+        //     }
+        // }
+
+
+// using While loop
+
+        // let n=prompt("Enter the number");
+        // let i=1;
+        // while(i<=n){
+
+        //     if(i%2==0){
+        //         console.log(i ,"Is Even number");
+        //     }else{
+        //         console.log(i ,"Is odd number");
+
+        //     }
+        //     i++
+        // }
+
+
+// count how many number are divisible by 3 and 5 in between 1 to 100
+
+//     let count=0;
+
+// for(let i=1;i<=100;i++){
+//     if(i%3==0 && i%5==0){
+//             console.log(i);
+
+//         count++
+        
+//     }
+// }
+//     console.log("total numbers",count);
+
+
+
+
