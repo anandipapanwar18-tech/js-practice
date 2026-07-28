9f84c0f4045a8b3754a072048790bdd1663a8941
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-07-28 23:17:13 +0530

    Practiced more examples

--- 06-Functions/Functions.js
+++ 06-Functions/Functions.js
@@ -65,3 +65,103 @@
 
         // let result =add(2,3);
         // console.log(result);
+
+
+
+// Create a function expression that checks whether a number is positive or negative.
+
+        // let PN= function(num){
+        //     if(num>0){
+        //         console.log("positive");
+        //     }else {
+        //         console.log("Negative");
+        //     }
+        // }
+
+
+        // PN(3)
+
+
+
+// Convert this into an arrow function.
+
+// function multiply(a,b){
+//     return a*b;
+// }
+
+        // let Mul=(a,b)=>{
+        //     return a*b;
+        // }
+
+        // console.log(Mul(2,3));
+
+
+// Write an arrow function that returns the cube of a number.
+
+        // let cube=(n)=>{
+        //     return n*n*n;
+        // }
+
+        // console.log(cube(2));
+
+
+// Write an arrow function to calculate BMI.
+// Formula
+// BMI = weight / (height * height)
+
+
+        // let BMI=(w,h)=>{
+        //     return w/(h*h);
+        // }
+
+        // let bmi=BMI(80,6.3);
+        // console.log(bmi);
+
+
+//         let calcBMI = (w, feet, inches) => {
+//     let totalInches = (feet * 12) + inches;
+//     let meters = totalInches * 0.0254;
+//     return w / (meters * meters);
+// };
+
+// let bmi = calcBMI(80, 6, 3); // 80 kg, 6 feet 3 inches
+// console.log(bmi);
+
+
+
+
+// Create a function
+// introduce(name, age)
+// Output :My name is Anandi and I am 21 years old.
+
+
+
+            // function introduce(name, age){
+            //     console.log(`my name is ${name} and i am ${age} years old.`);
+            // }
+
+            // introduce("anandi", 21);
+
+
+
+// Create a function with three parameters.
+// price
+// quantity
+// tax
+// Return the total bill.
+
+            // function product(price, quantity,tax){
+            //         return (price*quantity)+tax;
+            // }
+            // let bill=product(20,2,3);
+            // console.log(bill);
+
+
+
+// function demo(a,b){
+//     console.log(a,b);
+// }
+
+// demo(10)
+
+// output will be the 10 undefined
