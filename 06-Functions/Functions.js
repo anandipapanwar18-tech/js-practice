b9d5724317161b41bf53d9646737ebffe062d3e5
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-07-28 14:49:06 +0530

    practicing Function Examples

---
+++ 06-Functions/Functions.js
@@ -0,0 +1,67 @@
+// Create a function greet() that prints: Hello, Welcome!
+        // function greet(){
+        //     console.log("hello welcome ");
+        // }
+        //  greet();
+
+
+// Create a function square(num) that returns the square of a number.
+
+        // function square(num){
+        //     return num*num;
+        // }
+
+        // console.log(square(2));
+
+
+// Write a function isEven(num) that returns true if the number is even, otherwise false.
+
+        // function isEven(num){
+        //     if(num%2==0){
+        //         return true;
+
+        //     }
+        //     else{
+        //         return false;
+        //     }
+        // }
+
+        // let result= isEven(2);
+        // console.log(result);    
+
+
+// Create a function that returns the largest of two numbers.
+
+        //  function MaxValue(n, m){
+        //         if(n>m){
+        //             return n;
+        //         }else{
+        //             return m;
+        //         }
+        //     }
+        //     console.log(MaxValue(3,4));
+
+
+
+        
+    
+        // function MaxValue(n, m){
+        //     return Math.max(n,m);
+        // }
+        // console.log(MaxValue(3,4));
+
+
+
+// Write the following using a function expression.
+
+// function add(a,b){
+//     return a+b;
+// }
+
+
+        // let add=function(a,b){
+        //     return a+b
+        // }
+
+        // let result =add(2,3);
+        // console.log(result);
