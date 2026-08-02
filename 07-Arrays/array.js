aedcf63a60afe4a9d7f08481b6a10069da5ecf4f
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-08-02 22:49:55 +0530

    practicing

--- 07-Arrays/array.js
+++ 07-Arrays/array.js
@@ -21,3 +21,25 @@
             //    console.log(fruits[0])
             // console.log(fruits[1])
             // console.log(fruits[2])
+
+
+// Array length
+                // let num=[1,2,4,6,7];
+                // console.log(num.length);
+                // in this example index is 0 to 4 and length is 5
+
+
+// push() – Add to the end
+
+// let arr = [1, 2];
+// arr.push(3);
+// arr[3]=4;
+// arr[5]=4;
+
+// console.log(arr);
+
+        
+        // unshift() – Add to the beginning
+        // let arr = [1, 2];
+        // arr.unshift(5   );
+        // console.log(arr)
