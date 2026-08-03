7bcb1439479fee3a2f16490800cb40f99b3ad9e0
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-08-03 23:36:53 +0530

    practicing

--- 07-Arrays/array.js
+++ 07-Arrays/array.js
@@ -31,15 +31,44 @@
 
 // push() – Add to the end
 
-// let arr = [1, 2];
-// arr.push(3);
-// arr[3]=4;
-// arr[5]=4;
+        // let arr = [1, 2];
+        // arr.push(3);
+        // arr[3]=4;
+        // arr[5]=4;
 
-// console.log(arr);
+        // console.log(arr);
 
+
+
+// POP   pop() method removes the last element from an array and returns that element.
+                // let Arr=[1,2,3,4,5];
+                // Arr.pop();
+                // console.log(Arr);
+
+// Shift reomve element at first
+
+                // let flower=["Sunflower", "lily"];
+                // flower.shift();
+                // console.log(flower);
         
-        // unshift() – Add to the beginning
+ // unshift() – Add to the beginning
         // let arr = [1, 2];
         // arr.unshift(5   );
         // console.log(arr)
+
+
+
+// Loop Through an Array
+
+// Using for loop
+        //  let Arr=[1,2,3,4,5];
+        //        for(let i =0;i<Arr.length;i++){
+        //         console.log(Arr[i]);
+        //        }
+
+
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// for(let fruit of fruits){
+//     console.log(fruit);
+// }
