f7e79d20e8321ccbacd67fbd5ec271adb2e6df15
Anandi Papanwar <anandipapanwar18@gmail.com>
2026-08-06 14:17:37 +0530

    practicing abj

---
+++ 08-Objects/objects.js
@@ -0,0 +1,24 @@
+// An object is a collection of key-value pairs used to store related data.
+
+
+        // let obj={
+        //     name:"Prarik",
+        //     age :20,
+        //     pro :"student"
+        // }
+        // let a="name";
+        // console.log(obj[a])
+        // console.log(obj.age)
+
+
+
+// nested obj
+
+// let user={
+//     name:"anandi",
+//      person:{
+//         age:20
+//     }
+// }
+
+// console.log(user.person.age)
